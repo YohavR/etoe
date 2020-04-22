dba03f3b859dbc4365abb897fe3d08f503dc06b5
Yohav Ruimy <yohavr@macbook-pro-de-yohav.home>
2020-04-22 13:33:29 +0200

    Ajout et configuration  de FOSUserBundle

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,12 +2,15 @@
 // src/Controller/DefaultController.php
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class DefaultController
+
+class DefaultController extends AbstractController
 {
     public function index()
     {
-        return new Response('Hello!');
+        return $this->render('index.html.twig');
     }
 }
